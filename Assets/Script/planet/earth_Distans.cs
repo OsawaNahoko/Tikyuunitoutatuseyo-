605281ebe3ddd3cdb3d97eaac6d4ec2f509f4ec1
SioazinoNapoporitan <nahoko58osawa@gmail.com>
2024-12-03 23:34:31 +0900

    とりあえずの提出版

---
+++ Assets/Script/planet/earth_Distans.cs
@@ -0,0 +1,23 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class earth_Distans : MonoBehaviour
+// {
+//     [SerializeField] Text earthDistansText;
+//     void Start()
+//     {
+        
+//     }
+
+//     IEnumerator earthDistansCount()
+//     {
+//         NPCScoreText.text    = $"{Scoredata.NPCScore}";
+//         yield return null;
+
+//         if(globalData.GameOverFlag == true)
+//         {
+//             yield break;
+//         }
+//     }
+// }
