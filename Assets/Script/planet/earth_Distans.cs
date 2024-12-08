d38635e03b85d9680c31ea0df01402bf0a25dffa
SioazinoNapoporitan <nahoko58osawa@gmail.com>
2024-12-08 16:15:13 +0900

    小数点第二位まで表示させました。

--- Assets/Script/planet/earth_Distans.cs
+++ Assets/Script/planet/earth_Distans.cs
@@ -6,16 +6,18 @@ using UnityEngine.UI;
 public class earth_Distans : Base_planet
 {
     [SerializeField] Text earthDistansText;
-    decimal Distans = 100.0m;
+    decimal Distansdecimal = 100.0m;
 
     protected IEnumerator earthDistansCount(int Time,float Speed)
     {
         for(int i = 0; i < Time; i++)
         {
+        
+        Distansdecimal -= 0.1m;
+        float Distansfloat =(float)Distansdecimal;
 
-        earthDistansText.text  = $"{Distans}Km";
-        Distans -= 0.1m;
-        Debug.Log($"動いてる？？{Distans}");
+        earthDistansText.text  = $"{Distansfloat:F2}Km";
+
         yield return new WaitForSeconds(Speed);
 
         if(globalData.GameOverFlag == true)
